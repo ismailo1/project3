c7869acf5a2cd737fbbbe447f0e75014ace273dd
ismailo1 <i_omer7@hotmail.com>
2023-12-04 19:27:14 -0500

    ETL files

---
+++ Resources/create table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Salaries (
+    JobTitle CHAR (30),
+    EmploymentType CHAR (30),
+    ExpertiseLevel CHAR (30),
+	CompanyLocation CHAR (30),
+    SalaryinUSD DECIMAL(10, 2),
+    CompanySize CHAR (30),
+    Year DATE)
