99e885006d72302b86af9dfe7700b2ef578e0972
YargKlnc <yarg79@hotmail.com>
2023-12-04 19:51:43 -0500

    commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,25 @@
 // storing url for json data
 
-// console log to make sure the data is being read
+// console log to make sure the data is being read **only for testing**
+
+// fetcj json data using d3 and console log
 
 // creating a function to display dropdown menu
+    // creating dropdown menu with list of countries
+    // receiving all country names from data file
+    // getting each country name and appending to dropdown menu
 
+    // calling functions when first country is selected
+
+// creating a function when country is changed by user
+    // getting value of selection
+    // calling functions when country is changed
+
+// console log here
+
+// adding init() to check html page is working **only for testing**
+// init();
+
+// creating a function to display dropdown menu
+    // fetch json data using d3 and console log
+    // creating dropdown menu Expertise level
