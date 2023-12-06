47533c3009c3ed186188418d786e48218c24bd91
YargKlnc <yarg79@hotmail.com>
2023-12-05 20:02:46 -0500

    commit pseaduo code

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,45 +1,56 @@
 // storing url for json data
-url = 'http://127.0.0.1:5000/api/v1.0/salaries'
+// const url = 'http://127.0.0.1:5000/api/v1.0/salaries'
 // console log to make sure the data is being read **only for testing**
-d3.json(url).then(
-    // if promise is fulfilled:
-    (data) => console.log(data),
-    // if promise is rejected:
-    () => console.log('data failed to load')
-);
-// fetch json data using d3 and console log
+// console.log(url);
 
-// creating a function to display dropdown menu
-    // creating dropdown menu with list of countries
-    // receiving all country names from data file
+// fetch json data using d3 and console log
+// d3.json(url).then(function(data) {
+//     console.log(data);
+// });
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// creating an init function to display dropdown menu with list of countries receiving all country names from data file
+    // User selects Country from dropdown menu
+    // Creating a function for a Horizontal Bar Chart that dynamically updating upon selecting a country, unveiling the top 10 highest-paying job titles in ascending order
+    // Creating an interactive Map that evolves with country selection, offering a nuanced understanding of job title density and count globally
     // getting each country name and appending to dropdown menu
 
-    // calling functions when first country is selected
+    // creating an optionChanged function when country is changed by user and calling functions when country is changed
+    // getting value of selection and updating views of horizontal bar chart and interactive map
 
-// creating a function when country is changed by user
-    // getting value of selection
-    // calling functions when country is changed
+// console log init here
+// console.log("init");
 
-// console log here
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// adding init() to check html page is working **only for testing**
-// init();
-
-// creating a function to display dropdown menu
-    // fetch json data using d3 and console log
-    // creating dropdown menu Expertise level
-
-
-
-
-    // Creating a function for a Horizontal Bar Chart that dynamically updating upon selecting a country, unveiling the top 10 highest-paying job titles in ascending order
-
-    // Creating an interactive Map that evolves with country selection, offering a nuanced understanding of job title density and count globally
-
+// creating an init function to display dropdown menu with list of job titles receiving all job titles from data file
+    // User selects Job Title from dropdown menu
     // Creating a Bar Chart that dynamically updates upon selecting a job title, unveiling the top 10 highest-paying countries in ascending order
-
     // Creating a Line Graph that dynamically updates upon selecting a job title, unveiling the increase of salary level in percentage
+    // getting each job title and appending to dropdown menu
 
-    // Creating a Line Chart featuring two lines—salary against job title with individual salary dots, and a second line delineating the correlation between experience level and salary
+    // creating an optionChanged function when job title is changed by user and calling functions when job title is changed
+    // getting value of selection and updating views of bar chart and line graph
 
-    // Creating a Line chart that shows the correlation between experience level and salary
+// console log init here
+// console.log("init");
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// creating an init function to display dropdown menu with list of expertise levels receiving all expertise levels from data file
+    // User selects Expertise Level from dropdown menu
+    // Creating a function for a Line Chart featuring salary against job title with individual salary dots that dynamically updates upon selecting an expertise level
+    // Creating a line chart that shows the correlation between experience level and salary
+    // getting each expertise level and appending to dropdown menu
+
+    // creating an optionChanged function when expertise level is changed by user and calling functions when expertise level is changed
+    // getting value of selection and updating views of both line charts
+
+// console log init here
+// console.log("init");
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// adding init() 
+// init();
