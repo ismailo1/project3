a33637d84ea3ca3089682f7201e57cbc4efe1f3f
YargKlnc <yarg79@hotmail.com>
2023-12-06 12:55:16 -0500

    first part commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -39,13 +39,17 @@ console.log("init");
 
 // creating a function to be used to display horizontal bar chart and interactive map
 function firstchartvalues(passedcountry) {
+    // fetch json data using d3 and console log
     d3.json(url).then(function(data) {
+        console.log(data);
         // get country names data
         let countries = data["Company Location"];
-        // filter data by country selected
-        let filteredcountry = data.filter(row => row["Company Location"] === passedcountry);
-        let filteredcountrydata = filteredcountry[0];
+        // filter data for the country selected
+        let filteredcountry = countries.filter(country => country === passedcountry);
+        // get data for bar chart
+        let filteredcountrydata = filteredcountry[0].filteredcountrydata;
         console.log(filteredcountrydata);
+        
         // calling function to display horizontal bar chart
         horizontalbarchart(filteredcountrydata);
         // calling function to display interactive map
@@ -53,16 +57,78 @@ function firstchartvalues(passedcountry) {
     });
 };
 
+    // Creating a function for a Horizontal Bar Chart that dynamically updating upon selecting a country, unveiling the top 10 highest-paying (salaries) job titles in ascending order
+function horizontalbarchart(filteredcountrydata) {
+    console.log(filteredcountrydata);
+    
+    // Counting the total number of each job title
+    let jobTitleCount = {};
+    filteredcountrydata["Job Title"].forEach(function(jobTitle) {
+        jobTitleCount[jobTitle] = (jobTitleCount[jobTitle] || 0) + 1;
+    });
+    
+    // Creating the bar chart
+    let tracebarchart = {
+        x: Object.values(jobTitleCount),
+        y: Object.keys(jobTitleCount),
+        type: "bar",
+        orientation: "h"
+    };
+    let databarchart = [tracebarchart];
+    let layoutbarchart = {
+        title: "Top 10 Highest-Paying Job Titles in " + filteredcountrydata["Company Location"],
+        xaxis: {title: "Salary (USD)"},
+        yaxis: {title: "Job Title"}
+    };
+    Plotly.newPlot("bar", databarchart, layoutbarchart);
+};
+
+console.log(horizontalbarchart);
+
+    // Creating a function for an interactive Map that evolves with country selection, offering understanding of job title density and count globally
+function map(filteredcountrydata) {
+
+    // Adding a tile layer (the background map image) to our map
+    let streetmap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: `&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors`
+    });
+
+    let darkmap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: `Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.`
+    });
+
+    // defining a basemaps object to hold our base layers
+    let baseMaps = {
+        "Street Map": streetmap,
+        "Dark Map": darkmap
+    };
+        
+    // creating overlay object to hold our overlay layer
+    let overlayMaps = {
+        "Job Title Density": jobtitledensity
+    };
+    
+    // creating our map, giving it the streetmap and jobtitledensity layers to display on load
+    let myMap = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 2,
+        layers: [streetmap, jobtitledensity]
+    });
+            
+    // creating a layer control
+    // passing in our baseMaps and overlayMaps
+    // adding the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+};
+
+console.log("logic.js loaded");
+
 init();
 
-
-    // Creating a function for a Horizontal Bar Chart that dynamically updating upon selecting a country, unveiling the top 10 highest-paying job titles in ascending order
-    // Creating an interactive Map that evolves with country selection, offering a nuanced understanding of job title density and count globally
-    // User selects Country from dropdown menu
-
-
- 
     // getting value of selection and updating views of horizontal bar chart and interactive map
+
 
 // console log init here
 // console.log("init");
