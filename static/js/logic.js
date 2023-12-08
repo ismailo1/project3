af6d716fe7edca68885cdfdd3abb17b7e4534abf
YargKlnc <yarg79@hotmail.com>
2023-12-08 14:49:39 -0500

    commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,7 +4,7 @@ const url = 'http://127.0.0.1:5000/api/v1.0/salaries'
 console.log(url);
 
 // define myMap
-let myMap;
+// let myMap;
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -13,55 +13,94 @@ function init1() {
     // User selects Country from dropdown menu
     // getting each country name and appending to dropdown menu
     d3.json(url).then(function(data) {
-        // console.log(data);
-        // creating dropdown menu with list of Company Location
+        console.log(data);
+        // creating dropdown menu with list of Country
         let dropdown = d3.select("#selDataset");
-        // receiving all countries from Company Location in data file
+        // receiving all country names from countries in data file
         let countries = data["Company Location"];
         // getting each country name and appending to dropdown menu
         countries.forEach(function(country) {
-            dropdown.append("option").text(country).property("value", country);           
+            dropdown.append("option").text(country).property("value", country);
         });
         // calling function when first country is selected
         firstchartvalues(countries[0]);
-        console.log(countries[0]);       
-    }
-    )};
+        console.log(countries[0]);
+    });
+    };
+
 
 // creating an optionChanged function when country is changed by user and calling functions when country is changed
 function optionChanged(passedcountry) {
     firstchartvalues(passedcountry);
 };
 
-console.log("init1");
+// console.log("init1");
 
-// init1();
+init1();
 
-// creating a function to be used to display horizontal bar chart and interactive map
-// Move the horizontalbarchart function outside of the firstchartvalues function
-function horizontalbarchart(jobTitlesArray, passedcountry) {
-    // Counting the total number of each job title
-    let jobTitleCount = {};
-    jobTitlesArray.forEach(function(jobTitle) {
-        jobTitleCount[jobTitle] = (jobTitleCount[jobTitle] || 0) + 1;
+// // defining a function for first chart values to later be used to display horizontal bar chart that updates upon selecting a country, and x-axis giving top 10 highest paying salaries and y-axis giving these job titles in ascending order
+// function firstchartvalues(passedcountry) {
+//     // fetch json data using d3 and console log
+//     d3.json(url).then(function(data) {
+//         console.log(data);
+//         // filter data for the country selected
+//         let filteredcountrydata = data.Data.filter(function(entry) {
+//             return entry["Company Location"] === passedcountry;
+//         });
+//         if (filteredcountrydata.length > 0) {
+//             // Extracting job titles for the selected country
+//             let jobTitlesArray = filteredcountrydata.map(entry => entry["Job Title"]);
+//             // Extracting salaries for the selected country
+//             let salariesArray = filteredcountrydata.map(entry => entry["Salary"]);
+//             // calling function to display horizontal bar chart
+//             // horizontalbarchart(salariesArray, passedcountry);
+//         }
+//         else {
+//             console.log("No data available for the selected country.");
+//         }
+//     });
+// }
+
+// console.log(firstchartvalues);
+
+// // calling function to display horizontal bar chart
+// horizontalbarchart(salariesArray, passedcountry);
+
+
+// defining a function to be used to display a Horizontal Bar Chart that updates upon selecting a country, and x-axis giving top 10 highest paying salaries and y-axis giving these job titles in ascending order
+function horizontalbarchart(salariesArray, passedcountry) {
+    // Sorting salaries in descending order
+    salariesArray.sort((a, b) => b - a);
+    // Selecting the top 10 highest paying salaries
+    let topSalaries = salariesArray.slice(0, 10);
+    // Extracting job titles for the selected salaries
+    let jobTitlesArray = [];
+    topSalaries.forEach(function(salary) {
+        let index = salariesArray.indexOf(salary);
+        jobTitlesArray.push(jobTitlesArray[index]);
     });
-
     // Creating the bar chart
     let tracebarchart = {
-        x: Object.values(jobTitleCount),
-        y: Object.keys("Salary"),
+        x: topSalaries,
+        y: jobTitlesArray,
         type: "bar",
         orientation: "h"
     };
     let databarchart = [tracebarchart];
     let layoutbarchart = {
         title: "Top 10 Highest-Paying Job Titles in " + passedcountry,
-        xaxis: { title: "Salary (USD)" },
-        yaxis: { title: "Job Title" }
+        yaxis: { title: "Salary (USD)" },
+        xaxis: { title: "Job Title" }
     };
     Plotly.newPlot("bar", databarchart, layoutbarchart);
 }
 
+
+console.log(horizontalbarchart);
+// calling function to display horizontal bar chart
+horizontalbarchart(salariesArray, passedcountry);
+
+// Move the horizontalbarchart function outside of the firstchartvalues function
 function firstchartvalues(passedcountry) {
     // fetch json data using d3 and console log
     d3.json(url).then(function(data) {
@@ -87,105 +126,203 @@ function firstchartvalues(passedcountry) {
 
 console.log(horizontalbarchart);
 
-//function to create features to be used in the interactive map
-function createFeatures(datascienceData) {
-
-
-    // function to run once for each data in the array
-    function onEachFeature(data, layer) {
-        // giving each feature a popup describing the job title, company location, salary and expertise level
-        layer.bindPopup("<h3>" + data["Job Title"] + "</h3><hr><p>" + data["Company Location"] + "</p><hr><p>" + data["Salary"] + "</p><hr><p>" + data["Expertise Level"] + "</p>");
-    }
-
-    // creating a GeoJSON layer containing the features array on the datascienceData object
-    function pointToLayer(data, jobTitleCount) {
-        let options = {
-            radius: data["Salary"] / 10000,
-            fillColor: getColor(data["Salary"]),
-            color: getColor(data["Salary"]),
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8
-        };
-        return L.circleMarker(jobTitleCount, options);
-    }
-
-    // creating a variable for the GeoJSON layer
-    let datascience = L.geoJSON(datascienceData, {
-        onEachFeature: onEachFeature,
-        pointToLayer: pointToLayer
-    });
-
-    // sending the datascience layer to the createMap function
-    createMap(datascience);
-    
-}
-
-// creating a function to determine the color of the marker based on the salary
-function getColor(salary) {
-    switch (true) {
-        case salary > 200000:
-            return "#FF0000";
-        case salary > 150000:
-            return "#FFA500";
-        case salary > 100000:
-            return "#FFFF00";
-        case salary > 50000:
-            return "#008000";
-        case salary > 25000:
-            return "#0000FF";
-        default:
-            return "#4B0082";
-    }
-}
-
-
-
-// Creating a function for an interactive Map that evolves with country selection, offering understanding of job title density and count globally
-function createMap(datascience) {
-    console.log(datascience);
-
-    // Adding a tile layer (the background map image) to our map
-    let streetmap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: `&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors`
-    });
-
-    let darkmap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: `Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.`
-    });
-
-    // defining a basemaps object to hold our base layers
-    let baseMaps = {
-        "Street Map": streetmap,
-        "Dark Map": darkmap
-    };
-        
-    // creating overlay object to hold our overlay layer
-    let overlayMaps = {
-        "Job Title Density": jobtitledensity
-    };
-    
-    // creating our map, giving it the streetmap and jobtitledensity layers to display on load
-    let myMap = L.map("map", {
-        center: [37.09, -95.71],
-        zoom: 2,
-        layers: [streetmap, jobtitledensity]
-    });
-    
-            
-    // creating a layer control
-    // passing in our baseMaps and overlayMaps
-    // adding the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-    }).addTo(myMap);
-};
-
-// console.log(map);
-
-console.log("logic.js loaded");
+// calling function to display horizontal bar chart
+horizontalbarchart(salariesArray, passedcountry);
 
 init1();
+
+// console log salariesArray here
+console.log(salariesArray);
+
+// Move the horizontalbarchart function outside of the firstchartvalues function
+// function firstchartvalues(passedcountry) {
+//     // fetch json data using d3 and console log
+//     d3.json(url).then(function(data) {
+//         console.log(data);
+//         // filter data for the country selected
+//         let filteredcountrydata = data.Data.filter(function(entry) {
+//             return entry["Company Location"] === passedcountry;
+//         });
+
+//         if (filteredcountrydata.length > 0) {
+//             // Extracting job titles for the selected country
+//             let jobTitlesArray = filteredcountrydata.map(entry => entry["Job Title"]);
+            
+//             // calling function to display horizontal bar chart
+//             horizontalbarchart(jobTitlesArray, passedcountry);
+//             // // calling function to display interactive map
+//             // createMap(filteredcountrydata);
+//         } else {
+//             console.log("No data available for the selected country.");
+//         }
+//     });
+// }
+
+// console.log(horizontalbarchart);
+
+// init1();
+
+
+
+
+
+
+
+
+
+
+
+// function horizontalbarchart(jobTitlesArray, passedcountry) {
+//     // Counting the total number of each job title
+//     let jobTitleCount = {};
+//     jobTitlesArray.forEach(function(jobTitle) {
+//         jobTitleCount[jobTitle] = (jobTitleCount[jobTitle] || 0) + 1;
+//     });
+
+//     // Creating the bar chart
+//     let tracebarchart = {
+//         x: Object.values(jobTitleCount),
+//         y: Object.keys("Salary"),
+//         type: "bar",
+//         orientation: "h"
+//     };
+//     let databarchart = [tracebarchart];
+//     let layoutbarchart = {
+//         title: "Top 10 Highest-Paying Job Titles in " + passedcountry,
+//         xaxis: { title: "Salary (USD)" },
+//         yaxis: { title: "Job Title" }
+//     };
+//     Plotly.newPlot("bar", databarchart, layoutbarchart);
+// }
+
+// Move the horizontalbarchart function outside of the firstchartvalues function
+// function firstchartvalues(passedcountry) {
+//     // fetch json data using d3 and console log
+//     d3.json(url).then(function(data) {
+//         console.log(data);
+//         // filter data for the country selected
+//         let filteredcountrydata = data.Data.filter(function(entry) {
+//             return entry["Company Location"] === passedcountry;
+//         });
+
+//         if (filteredcountrydata.length > 0) {
+//             // Extracting job titles for the selected country
+//             let jobTitlesArray = filteredcountrydata.map(entry => entry["Job Title"]);
+            
+//             // calling function to display horizontal bar chart
+//             horizontalbarchart(jobTitlesArray, passedcountry);
+//             // // calling function to display interactive map
+//             // createMap(filteredcountrydata);
+//         } else {
+//             console.log("No data available for the selected country.");
+//         }
+//     });
+// }
+
+// console.log(horizontalbarchart);
+
+// define jobtitlesdensity
+// let jobtitledensity;
+
+// //function to create features to be used in the interactive map
+// function createFeatures(datascienceData) {
+   
+
+//     // function to run once for each data in the array
+//     function onEachFeature(data, layer) {
+//         // giving each feature a popup describing the job title, company location, salary and expertise level
+//         layer.bindPopup("<h3>" + data["Job Title"] + "</h3><hr><p>" + data["Company Location"] + "</p><hr><p>" + data["Salary"] + "</p><hr><p>" + data["Expertise Level"] + "</p>");
+//     }
+
+//     // creating a GeoJSON layer containing the features array on the datascienceData object
+//     function pointToLayer(data, jobTitleCount) {
+//         let options = {
+//             radius: data["Salary"] / 10000,
+//             fillColor: getColor(data["Salary"]),
+//             color: getColor(data["Salary"]),
+//             weight: 1,
+//             opacity: 1,
+//             fillOpacity: 0.8
+//         };
+//         return L.circleMarker(jobTitleCount, options);
+//     }
+
+//     // creating a variable for the GeoJSON layer
+//     let datascience = L.geoJSON(datascienceData, {
+//         onEachFeature: onEachFeature,
+//         pointToLayer: pointToLayer
+//     });
+
+//     // sending the datascience layer to the createMap function
+//     createMap(datascience);
+    
+// }
+
+// // creating a function to determine the color of the marker based on the salary
+// function getColor(salary) {
+//     switch (true) {
+//         case salary > 200000:
+//             return "#FF0000";
+//         case salary > 150000:
+//             return "#FFA500";
+//         case salary > 100000:
+//             return "#FFFF00";
+//         case salary > 50000:
+//             return "#008000";
+//         case salary > 25000:
+//             return "#0000FF";
+//         default:
+//             return "#4B0082";
+//     }
+// }
+
+
+
+// // Creating a function for an interactive Map that evolves with country selection, offering understanding of job title density and count globally
+// function createMap(datascience) {
+//     console.log(datascience);
+
+//     // Adding a tile layer (the background map image) to our map
+//     let streetmap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//         attribution: `&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors`
+//     });
+
+//     let darkmap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//         attribution: `Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.`
+//     });
+
+//     // defining a basemaps object to hold our base layers
+//     let baseMaps = {
+//         "Street Map": streetmap,
+//         "Dark Map": darkmap
+//     };
+        
+//     // creating overlay object to hold our overlay layer
+//     let overlayMaps = {
+//         "Job Title Density": jobtitledensity
+//     };
+    
+//     // creating our map, giving it the streetmap and jobtitledensity layers to display on load
+//     let myMap = L.map("map", {
+//         center: [37.09, -95.71],
+//         zoom: 2,
+//         layers: [streetmap, jobtitledensity]
+//     });
+    
+            
+//     // creating a layer control
+//     // passing in our baseMaps and overlayMaps
+//     // adding the layer control to the map
+//     L.control.layers(baseMaps, overlayMaps, {
+//         collapsed: false
+//     }).addTo(myMap);
+// };
+
+// // console.log(map);
+
+// console.log("logic.js loaded");
+
+// init1();
 
 
 
